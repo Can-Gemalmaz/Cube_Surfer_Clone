97693c2f1f160c9f65aeef019151152611f99fec
Can-Gemalmaz <cangemcan@gmail.com>
2024-08-22 22:00:18 +0300

    Update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Button replayButton;
+    [SerializeField] Button quitButton;
+    [SerializeField] GameObject gameMenu;
+
+    PlayerCubeManager cubeManager;
+    PlayerDiamondManager diamondManager;
+
+
+    private void Awake()
+    {
+        replayButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            gameMenu.SetActive(false);
+
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+
+    private void Start()
+    {
+        cubeManager = PlayerCubeManager.Instance;
+
+        diamondManager = cubeManager.GetComponent<PlayerDiamondManager>();
+        cubeManager.OnPlayerDead += OnPlayerDeadEvent;
+    }
+
+    private void OnPlayerDeadEvent()
+    {
+        diamondManager.SavePrefs(diamondManager.GetDiamondCount());
+        gameMenu.SetActive(true);
+    }
+}
